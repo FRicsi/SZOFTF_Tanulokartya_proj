9db202a622bcbf7badec0de9d4a13995822e1705
FRicsi <fenyo.r@gmail.com>
2026-05-09 19:26:30 +0200

    chore: configure testing environment with jsdom and setup file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/SZOFTF_Tanulokartya_proj/',
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    globals: true,
+  },
 })
